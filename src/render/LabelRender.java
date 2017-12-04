4c5c814c4ee60327df3f5d8d9b8c519429ceb4bd
MMK <MMK@LAPTOP-TMH3FC22>
2017-12-05 06:47:52 +0800

    201712050647

--- src/render/LabelRender.java
+++ src/render/LabelRender.java
@@ -1,21 +1,34 @@
 package render;
 
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.Shape;
 
 import gdecid.visual.tuple.TableVisualItem;
+import prefuse.visual.VisualItem;
 
 public class LabelRender {
 	
+	String imageLoc;
+	
 	public void Render(Graphics2D g, TableVisualItem item) {
 		String text = item.getName();
-		double x = item.getX();
-		double y = item.getY();
-		drawString(g, text, x, y);
+		Image  img  = getImage(item);
+		double tx = item.getX();
+		double ty = item.getY();
+		drawString(g, text, tx, ty);
 	}
 	
 	public void drawString(Graphics2D g, String text, double x, double y) {
 		g.drawString(text, (float)x, (float)y);
 	}
+	
+	public void drawImage(Graphics2D g) {
+		g.drawImage(img, (int)ix, (int)iy, null);
+	}
+	
+    protected Image getImage(VisualItem item) {
+
+    }
 	
 }
