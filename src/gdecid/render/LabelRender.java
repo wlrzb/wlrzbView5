f04533cb60df14d3132bc148a15afbea6dfae8e9
GdecidHang <GdecidHang@DESKTOP-GU2B5PT>
2017-12-06 14:34:56 +0800

    201712061434

--- src/gdecid/render/LabelRender.java
+++ src/gdecid/render/LabelRender.java
@@ -1,5 +1,6 @@
 package gdecid.render;
 
+import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Shape;
@@ -16,33 +17,48 @@ import gdecid.visual.tuple.TableVisualItem;
 
 public class LabelRender {
 	
+	protected int m_imageMargin = 20;  // 图片与文字之间的空隙
+	
+	protected Font  m_font;
+	
 	private static Image img;
+	
+	
 	static {
-		//img=ImageIO.read(LabelRender.class.getClassLoader().getResourceAsStream("images/ren.png"));
-		URL imageURL = IOLib.urlFromString("images/ren.png"); 
-		img=Toolkit.getDefaultToolkit().createImage(imageURL);
+		try {
+			img=ImageIO.read(LabelRender.class.getClassLoader().getResourceAsStream("images/ren30.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//URL imageURL = IOLib.urlFromString("images/ren.png"); 
+		//img=Toolkit.getDefaultToolkit().createImage(imageURL);
 	}
 	
 	AffineTransform m_transform = new AffineTransform();
 	
 	public void Render(Graphics2D g, TableVisualItem item) {
 		String text = item.getName();
+		double size = item.getSize(); // 获取图片的缩放倍数
+		m_font = item.getFont();      // 获取文字的字体
+		
 		double tx = item.getX();
 		double ty = item.getY();
 		double ix = item.getX();
 		double iy = item.getY();
-		drawImage(g, img, ix, iy);
-		//drawString(g, text, tx, ty);
-		//g.drawImage(img, m_transform, (ImageObserver) this);
+		
+		// 计算图片的高度和宽度，这里要用图片的原始高度乘图片的缩放倍数。
+        double iw = size * img.getWidth(null);
+        double ih = size * img.getHeight(null);
+        
+        ty = iy + ih + m_imageMargin;
+
+        m_transform.setTransform(size, 0, 0, size, ix, iy);
+        g.drawImage(img, m_transform, null);
+        
+        
+        g.setFont(m_font);  // 设置画笔的字体
+        g.drawString(text, (float)tx, (float)ty);
 	}
-	
-	public void drawString(Graphics2D g, String text, double x, double y) {
-		g.drawString(text, (float)x, (float)y);
-	}
-	
-	public void drawImage(Graphics2D g, Image img, double ix, double iy) {
-		g.drawImage(img, (int)ix, (int)iy, null);
-	}
-	
 	
 }
--- src/gdecid/visual/tuple/TableVisualItem.java
+++ src/gdecid/visual/tuple/TableVisualItem.java
@@ -1,5 +1,6 @@
 package gdecid.visual.tuple;
 
+import java.awt.Font;
 import java.awt.Graphics;
 
 import gdecid.visual.VisualItem;
@@ -8,6 +9,8 @@ public class TableVisualItem implements VisualItem{
 	String name;
 	double x;
 	double y;
+	double size = 1.0; //定义图片的缩放大小。
+	Font font = new Font("黑体", Font.PLAIN, 15);;
 	
 	public TableVisualItem(String name) {
 		this.name = name;
@@ -36,6 +39,22 @@ public class TableVisualItem implements VisualItem{
 	public void render(Graphics g) {
 		// TODO Auto-generated method stub
 		
+	}
+
+	public double getSize() {
+		return size;
+	}
+
+	public void setSize(double size) {
+		this.size = size;
+	}
+
+	public Font getFont() {
+		return font;
+	}
+
+	public void setFont(Font font) {
+		this.font = font;
 	}
 
 	
